0d15baa1b94b6b04759400afffd206641fad8343
Vinh Vu <EMAIL>
2023-11-10 11:36:11 -0800

    Mutex pt. 2

--- src/main/scala/com/rockthejvm/part4coordination/Mutex.scala
+++ src/main/scala/com/rockthejvm/part4coordination/Mutex.scala
@@ -1,5 +1,6 @@
 package com.rockthejvm.part4coordination
 
+import cats.effect.kernel.Outcome.{Canceled, Errored, Succeeded}
 import cats.effect.{Deferred, IO, IOApp, Ref}
 import cats.syntax.parallel.*
 import com.rockthejvm.utils.*
@@ -20,7 +21,35 @@ object Mutex {
 
   val unlocked = State(locked = false, Queue())
 
-  def create: IO[Mutex] = Ref[IO].of(unlocked).map { refState =>
+  def create: IO[Mutex] = Ref[IO].of(unlocked).map(createMutexWithCancellation)
+
+  def createMutexWithCancellation(refState: Ref[IO, State]): Mutex =
+    new Mutex {
+      override def acquire: IO[Unit] = IO.uncancelable { poll =>
+        Deferred[IO, Unit].flatMap { signal =>
+          val cleanup = refState.modify { case State(locked, queue) =>
+            val newQueue = queue.filterNot(_ eq signal)
+            State(locked, newQueue) -> release
+          }.flatten
+
+          refState.modify {
+            case State(false, _)    => State(true, Queue())         -> IO.unit
+            case State(true, queue) => State(true, queue :+ signal) -> poll(signal.get).onCancel(cleanup)
+          }.flatten
+        }
+      }
+
+      override def release: IO[Unit] =
+        refState.modify {
+          case state @ State(false, _)             => unlocked -> IO.unit
+          case State(true, queue) if queue.isEmpty => unlocked -> IO.unit
+          case State(true, queue) =>
+            val (signal, newQueue) = queue.dequeue
+            State(true, newQueue) -> signal.complete(()).void
+        }.flatten
+    }
+
+  def createSimpleMutex(refState: Ref[IO, State]): Mutex =
     new Mutex {
       /*
         Change the state of the Ref:
@@ -33,20 +62,6 @@ object Mutex {
           case State(true, queue) => State(true, queue :+ signal) -> signal.get
         }.flatten
       }
-      // for {
-      //   state <- refState.get
-      //   result <- state match {
-      //     case State(false, queue) => IO((State(true, queue), Option.empty[Signal]))
-      //     case State(true, queue) =>
-      //       Deferred[IO, Unit].map(signal => (State(true, queue :+ signal), Option(signal)))
-      //   }
-      //   (newState, optionalSignal) = result
-      //   _ <- optionalSignal match {
-      //     case None         => IO.unit
-      //     case Some(signal) => signal.get
-      //   }
-      //   _ <- refState.set(newState)
-      // } yield ()
 
       /*
         Change the state of the Ref:
@@ -63,31 +78,14 @@ object Mutex {
             val (signal, newQueue) = queue.dequeue
             State(true, newQueue) -> signal.complete(()).void
         }.flatten
-        // for {
-        //   state <- refState.get
-        //   result = state match {
-        //     case state @ State(false, queue)         => (state, Option.empty[Signal])
-        //     case State(true, queue) if queue.isEmpty => (State(false, queue), Option.empty[Signal])
-        //     case State(true, queue) =>
-        //       val (signal, newQueue) = queue.dequeue
-        //       (State(true, newQueue), Option(signal))
-        //   }
-        //   (newState, optionalSignal) = result
-        //   _ <- optionalSignal match {
-        //     case None         => IO.unit
-        //     case Some(signal) => signal.complete(())
-        //   }
-        //   _ <- refState.set(newState)
-        // } yield ()
     }
-  }
 }
 
 object MutexPlayground extends IOApp.Simple {
 
   def criticalTask(): IO[Int] = IO.sleep(1.second) >> IO(Random.nextInt(100))
 
-  def createNonLockingTask(id: Int, mutex: Mutex): IO[Int] = for {
+  def createLockingTask(id: Int, mutex: Mutex): IO[Int] = for {
     _ <- IO(s"[task $id] waiting for permission...").debugs
     _ <- mutex.acquire // blocks if the mutex has been acquired by some other fiber
     // critical section
@@ -101,9 +99,28 @@ object MutexPlayground extends IOApp.Simple {
 
   def demoNonLockingTask(): IO[List[Int]] = for {
     mutex   <- Mutex.create
-    results <- (1 to 10).toList.parTraverse(id => createNonLockingTask(id, mutex))
+    results <- (1 to 10).toList.parTraverse(id => createLockingTask(id, mutex))
   } yield results
   // only one task can proceed at a task
 
-  override def run: IO[Unit] = demoNonLockingTask().debugs.void
+  def createCancellingTask(id: Int, mutex: Mutex): IO[Int] =
+    if (id % 2 == 0) createLockingTask(id, mutex)
+    else
+      for {
+        fib <- createLockingTask(id, mutex).onCancel(IO("s[task $id] received cancellation!").debugs.void).start
+        _   <- IO.sleep(2.seconds) >> fib.cancel
+        out <- fib.join
+        result <- out match {
+          case Succeeded(effect) => effect
+          case Errored(_)        => IO(-1)
+          case Canceled()        => IO(-2)
+        }
+      } yield result
+
+  def demoCancellingTasks() = for {
+    mutex   <- Mutex.create
+    results <- (1 to 10).toList.parTraverse(id => createCancellingTask(id, mutex))
+  } yield results
+
+  override def run: IO[Unit] = demoCancellingTasks().debugs.void
 }
